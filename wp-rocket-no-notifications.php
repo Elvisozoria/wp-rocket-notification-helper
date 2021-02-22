862868f6bbc887dbd43d7ab721ff494655918b1b
Elvis Ozoria <elvis.ozoria@gmail.com>
2021-02-22 15:28:27 -0400

    also hide warnings from the cache dir

--- wp-rocket-no-notifications.php
+++ wp-rocket-no-notifications.php
@@ -11,6 +11,12 @@ function hide_rocket_warning_htaccess_permissions() {
 	remove_action( 'admin_notices', 'rocket_warning_htaccess_permissions' );
 }
 
+function hide_rocket_warning_cache_dir_permissions() {
+	remove_action( 'admin_notices', 'rocket_warning_cache_dir_permissions' );
+}
+
+
+add_action( 'admin_head', 'rocket_warning_cache_dir_permissions',1 );
 add_action('admin_head','hide_rocket_warning_htaccess_permissions', 1);
 
 
