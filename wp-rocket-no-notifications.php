f892115abb6f518f09f78c8e37d149f4c9e17288
Elvis Ozoria <elvis.ozoria@gmail.com>
2021-02-22 15:20:47 -0400

    basic funcionality added, now let's add the rest'

---
+++ wp-rocket-no-notifications.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: hide wp-rocket htaccess notifications
+ * Description: let's get this to work!
+ * License:     GNU General Public License v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+
+function hide_rocket_warning_htaccess_permissions() {
+	remove_action( 'admin_notices', 'rocket_warning_htaccess_permissions' );
+}
+
+add_action('admin_head','hide_rocket_warning_htaccess_permissions', 1);
+
+
